fac298826ac595fc2224625dd78a4c604b1b8f9c
Дмитрий Кривоченко <d.krivochenko@revvy.ru>
2024-11-10 19:13:33 +0300

    add taiga-ui

--- frontent/src/app/app.component.ts
+++ frontent/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import {TuiButton, TuiRoot} from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot, TuiButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
