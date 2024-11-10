c9b2ceda67ecde73e434247429a373fbc0e5ca1a
evildre <musaew97@gmail.com>
2024-11-10 21:32:28 +0300

    deleted first query

--- Database/initialising.py
+++ Database/initialising.py
@@ -8,19 +8,6 @@ password = "12345678"     # Пароль для входа
 # Создаем драйвер для подключения к базе данных
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-# Функция для выполнения запроса
-def create_and_query_database():
-    with driver.session() as session:
-        # Создаем узел в базе данных
-        session.run("CREATE (n:Person {name: 'Alice'})")
-        
-        # Запрашиваем узел
-        result = session.run("MATCH (n:Person) RETURN n.name AS name")
-        for record in result:
-            print(record["name"])
-
-# Запускаем функцию
-create_and_query_database()
 
 # Закрываем драйвер после работы с базой
 driver.close()
