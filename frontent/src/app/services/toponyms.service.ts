64c48a913b41829843b4ad91a840fbfb153edebc
Дмитрий Кривоченко <d.krivochenko@revvy.ru>
2024-11-11 17:58:35 +0300

    replaced dummy call to backend call

--- frontent/src/app/services/toponyms.service.ts
+++ frontent/src/app/services/toponyms.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {FilterDto, ToponymDto} from "../dtos/dtos";
-import {Observable, of} from "rxjs";
+import {catchError, Observable, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,37 +10,14 @@ export class ToponymsService {
 
   constructor(private readonly http: HttpClient) { }
 
-  getToponyms(filters: FilterDto) : Observable<ToponymDto[]> {
-    // call to this.http to backend.
-    console.log(filters);
-    return of([
-      {
-        name: 'Собор Святого Исаакия',
-        renameYears: [new Date('1922-01-01'), new Date('1950-05-01')],
-        address: 'Исаакиевская площадь, Санкт-Петербург, Россия',
-        photoUrl: 'https://placehold.co/600x400/EEE/31343C',
-        type: 'Собор',
-        style: 'Неоклассицизм',
-        architect: 'Огюст Монферран',
-      },
-      {
-        name: 'Эрмитаж',
-        renameYears: [new Date('1917-01-01')],
-        address: 'Дворцовая площадь, Санкт-Петербург, Россия',
-        photoUrl: 'https://placehold.co/600x400/EEE/31343C',
-        type: 'Музей',
-        style: 'Барокко',
-        architect: 'Бартоломео Растрелли',
-      },
-      {
-        name: 'Петропавловская крепость',
-        renameYears: [new Date('1703-05-16')],
-        address: 'Заячий остров, Санкт-Петербург, Россия',
-        photoUrl: 'https://placehold.co/600x400/EEE/31343C',
-        type: 'Крепость',
-        style: 'Петровское барокко',
-        architect: 'Доменико Трезини',
-      },
-    ]);
+  getToponyms(filters: FilterDto): Observable<ToponymDto[]> {
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.post<ToponymDto[]>('http://localhost:5001/api/toponyms', filters, { headers })
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching toponyms:', error);
+          return throwError(error);
+        })
+      );
   }
 }
