28f48f643fb2d3956acff5067398a1dc7fbf8b6a
Дмитрий Кривоченко <dima.krivochenko@mail.ru>
2024-12-19 17:44:06 +0300

    added handle to backend

--- frontent/src/app/services/toponyms.service.ts
+++ frontent/src/app/services/toponyms.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {FilterDto, ToponymDto} from "../dtos/dtos";
-import {catchError, Observable, of, throwError} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { FilterDto, ToponymDto } from "../dtos/dtos";
+import { catchError, Observable, throwError } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,6 @@ export class ToponymsService {
   constructor(private readonly http: HttpClient) { }
 
   getToponyms(filters: FilterDto): Observable<ToponymDto[]> {
-    console.log(filters);
     const headers = { 'Content-Type': 'application/json' };
     return this.http.post<ToponymDto[]>('http://localhost:5001/api/toponyms', filters, { headers })
       .pipe(
@@ -20,5 +19,32 @@ export class ToponymsService {
           return throwError(error);
         })
       );
+  }
+
+  import(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const uploadUrl = 'http://localhost:5001/api/import';
+
+    return this.http.post(uploadUrl, formData)
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching toponyms:', error);
+          return throwError(error);
+        })
+      );
+  }
+
+  export(): Observable<Blob> {
+    const downloadUrl = 'http://localhost:5001/api/export';
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.get(downloadUrl, { headers, responseType: 'blob' }).pipe(
+      catchError(error => {
+        console.error('Error downloading CSV:', error);
+        return throwError(error);
+      })
+    );
   }
 }
